e2d117a75827ebebbe2e8ce6d17ceaa8b31667b3
kmelter <kmelter97@gmail.com>
2022-03-11 16:43:21 -0600

    handle pagination and search errors

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,17 @@
 var issueContainerEl = document.querySelector("#issues-container");
 
+var displayWarning = function(repo) {
+    //add text to warning container
+    limitWarning.textContent = "To see more than 30 issues, visit ";
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+};
+
 var displayIssues = function(issues) {
     if (issues.length === 0) {
         issueContainerEl.textContent = "This repo has ho open issues!";
@@ -47,6 +59,11 @@ var getRepoIssues = function(repo) {
             response.json().then(function(data) {
                 //pass response data to dom funciton
                 displayIssues(data);
+
+                //check if api has paginated issues
+                if (response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
             });
         }
         else {
